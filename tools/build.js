d223d125edbe702235bfe70be42aeda029ce5d20
Dmitry Sorin <dsorin@yandex-team.ru>
2016-10-04 14:37:35 +0300

    Add assets, update repo

--- tools/build.js
+++ tools/build.js
@@ -9,46 +9,66 @@ const headerTpl = fs.readFileSync(`${__dirname}/../templates/header.hbs`, {encod
 const listTpl = fs.readFileSync(`${__dirname}/../templates/list.hbs`, {encoding: 'utf-8'});
 const simpleListTpl = fs.readFileSync(`${__dirname}/../templates/simple-list.hbs`, {encoding: 'utf-8'});
 const codeTpl = fs.readFileSync(`${__dirname}/../templates/code.hbs`, {encoding: 'utf-8'});
+const coverTpl = fs.readFileSync(`${__dirname}/../templates/cover.hbs`, {encoding: 'utf-8'});
 
 const wrapper = handlebars.compile(wrapperTpl);
 const header = handlebars.compile(headerTpl);
 const list = handlebars.compile(listTpl);
 const simpleList = handlebars.compile(simpleListTpl);
 const code = handlebars.compile(codeTpl);
+const cover = handlebars.compile(coverTpl);
 
 let slidesHTML = '';
 for (let slide of presentation.slides) {
-    slide.elements.forEach((element, i) => {
-        const data = {
-            title: slide.title,
-            slides: slide.elements.map((element, j) => {
-                const text = (typeof element === 'object')
-                    ? element.text
-                    : element;
+    switch (slide.type) {
+        case 'list':
+            slide.elements.forEach((element, i) => {
+                const data = {
+                    title: slide.title,
+                    slides: slide.elements.map((element, j) => {
+                        const text = (typeof element === 'object')
+                            ? element.text
+                            : element;
 
-                return {
-                    text,
-                    active: (i === j)
+                        return {
+                            text,
+                            active: (i === j)
+                        };
+                    })
                 };
-            })
-        };
 
-        slidesHTML += list(data);
+                slidesHTML += list(data);
 
-        if (typeof element === 'object') {
-            if (element.insiders) {
-                slidesHTML += simpleList({
-                    title: `${slide.title} > ${element.text}`,
-                    elements: element.insiders
-                });
-            } else if (element.code) {
-                slidesHTML += code({
-                    title: `${slide.title} > ${element.text}`,
-                    code: element.code
-                });
-            }
-        }
-    });
+                if (typeof element === 'object') {
+                    if (element.insiders) {
+                        slidesHTML += simpleList({
+                            title: `${slide.title} > ${element.text}`,
+                            elements: element.insiders
+                        });
+                    } else if (element.code) {
+                        slidesHTML += code({
+                            title: `${slide.title} > ${element.text}`,
+                            code: element.code
+                        });
+                    } else if (element.cover) {
+                        slidesHTML += cover({
+                            url: element.cover
+                        });
+                    }
+                }
+            });
+
+            break;
+
+        case 'cover':
+            slidesHTML += cover({
+                url: slide.image
+            });
+
+            break;
+    }
+
+
 }
 
 const headerHTML = header({
@@ -61,5 +81,5 @@ fs.writeFileSync(`${__dirname}/../index.html`, wrapper({
     title: `${presentation.header.title} &mdash; ${presentation.header.author.name}`,
     header: headerHTML,
     slides: slidesHTML,
-    repo: '1999/dom'
+    repo: '1999/autotests-qa-2016'
 }));
